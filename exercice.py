f66ffd980c0306776d42c45deb6faee59f4318de
felix <6244794@cegepoutaouais.qc.ca>
2025-03-31 21:44:03 -0500

    ajout de mouvement comme classmethod

--- exercice.py
+++ exercice.py
@@ -1,13 +1,25 @@
-from exercicesvalide import Exercicesvalide
     # ajouter intensiter pour chaque exercices pour ensuite pouvoir la calculer dans séance
     # a l'aide d'un calcule
 class Exercice:
+    mouvement = {
+        "bench": "Exercice de musculation ciblant principalement les pectoraux, effectué avec une barre ou des haltères.",
+        "squat": "Mouvement polyarticulaire sollicitant les jambes et les fessiers, souvent réalisé avec une barre sur les épaules.",
+        "deadlift": "Exercice de force sollicitant le dos, les jambes et les bras, consistant à soulever une barre depuis le sol.",
+        "pull-up": "Traction à la barre pour renforcer le dos et les bras en tirant son propre poids vers le haut.",
+        "shoulder_press": "Développement des épaules avec une barre ou des haltères en poussant le poids au-dessus de la tête.",
+        "bicep_curl": "Mouvement d'isolation pour les biceps, réalisé avec des haltères ou une barre en flexion des coudes.",
+        "triceps_dips": "Exercice au poids du corps ciblant les triceps en descendant et remontant sur des barres parallèles.",
+        "lunges": "Fentes pour travailler les jambes et les fessiers, réalisées avec ou sans charge supplémentaire.",
+        "plank": "Exercice de gainage renforçant les abdominaux et le tronc en maintenant une position statique.",
+        "leg_press": "Exercice sur machine pour muscler les jambes en poussant une plateforme avec les pieds."
+    }
+
     def __init__(self, nomexercice:str, rpe:int, set : int ,  rep : int,   poid: int,  temps: int = None):
-        if nomexercice in [e.value for e in Exercicesvalide]:
+        if nomexercice in Exercice.mouvement:
+
             self.nomexercice = nomexercice
         else:
-            raise ValueError("exercice invalide")
-        self.nomexercice = nomexercice
+            print("exercice invalide veuillez l'ajouter a liste d'exercices")
         self.rpe = rpe
         self.rep = rep
         self.set = set
@@ -15,12 +27,16 @@ class Exercice:
         self.temps = temps
 
 
+    @classmethod
+    def ajouter_mouvement(cls,nom:str, description:str):
+        Exercice.mouvement[nom] = description
+
 
 
 
     def __str__(self):
         if self.temps == None:
-            return f"{self.nomexercice} rep {self.rep} poid {self.poid}"
+            return f"{self.nomexercice} rpe {self.rpe} set {self.set} rep {self.rep} poid {self.poid}"
         else:
-            return f"{self.nomexercice} rep {self.rep} poid {self.poid} temps {self.temps}"
+            return f"{self.nomexercice} rpe {self.rpe} set {self.set}  rep {self.rep} poid {self.poid} temps {self.temps}"
 
