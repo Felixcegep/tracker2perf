56d8e38dad1a0af518f05c4c7f2678a9fa317212
loic <2050096@cegepoutaouais.qc.ca>
2025-04-04 14:39:24 -0400

    aliment

--- Muscledispo.py
+++ Muscledispo.py
@@ -1,3 +1,15 @@
 from enum import Enum
 class Muscledispo(Enum):
-    print("mettre muscle ici")
+
+    #Liste générée par AI
+    Pectoraux = "Pectoraux"
+    Biceps = "Biceps"
+    Triceps = "Triceps"
+    Dorsaux = "Dorsaux"
+    Quadriceps = "Quadriceps"
+    IschioJambiers = "Ischio-Jambiers"
+    Abdominaux = "Abdominaux"
+    Trapèzes = "Trapèzes"
+    Fessiers = "Fessiers"
+    Deltoïdes = "Deltoïdes"
+    Mollets = "Mollets"
