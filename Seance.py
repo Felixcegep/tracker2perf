88302ebd3e4a6df39c3220f2c2157e5907f08cb6
6244794 <6244794@cegepoutaouais.qc.ca>
2025-04-06 12:35:32 +0000

    correction de bug et ajout de suppeimer_exercice comme methode dinstance

--- Seance.py
+++ Seance.py
@@ -14,17 +14,24 @@ class Seance:
         Seance.nbseancetotal += 1
 
     def afficher_exercices(self):
-        for exercice in self.exercice_seaces:
-            print(exercice.nomexercice)
+        if len(self.exercice_seaces) == 0:
+            print("il n'y a pas d'exercice dans cette seance")
+        else:
+            compteur = 0
+            for exercice in self.exercice_seaces:
+                print(compteur, ":", exercice.nomexercice)
+                compteur += 1
 
     def ajouter_exercice(self, exercice:ExerciceMusculation):
         if isinstance(exercice, ExerciceCardio) or isinstance(exercice, ExerciceMusculation):
             self.exercice_seaces.append(exercice)
         else:
             raise ValueError("exercice n'est pas un ExerciceMusculation ou ExerciceCardio")
-
+    def supprimer_exercice(self, nom_exercice:str):
+        for exercice in self.exercice_seaces:
+            if exercice.nomexercice == nom_exercice:
+                self.exercice_seaces.remove(exercice)
+                break
+        print("nom invalide")
     def __str__(self):
         return f'{self.nom} {self.exercice_seaces}'
-exercice1 = ExerciceMusculation("test",1,1,1,1)
-seance1 = Seance("test")
-seance1.ajouter_exercice(exercice1)
