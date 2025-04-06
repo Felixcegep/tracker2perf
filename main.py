2a048ada964d34c4387e714a127655a798cfd2fc
6244794 <6244794@cegepoutaouais.qc.ca>
2025-04-06 18:22:55 +0000

    ajout de test

--- main.py
+++ main.py
@@ -17,17 +17,23 @@ if __name__ == '__main__':
 # seance
 
     seance1 = Seance("seance1")
+    seance2 = Seance("seance2")
+    
 # exercice
 
     Exercice1 = ExerciceMusculation("test", 1,1,2,3)
     Exercice2 = ExerciceMusculation("Presse à cuisses", 1,1,2,3)
-
+    Exercice3 = ExerciceMusculation("Presse à cuisses",5,5,5,5)
 # ajouter seance
     seance1.ajouter_exercice(Exercice1)
     seance1.ajouter_exercice(Exercice2)
+    seance2.ajouter_exercice(Exercice3)
     
 # jours
     day1.ajouter_seance(seance1)
+    day2.ajouter_seance(seance2)
+    
     joueur1.ajouter_journee(day1)
-
+    joueur1.ajouter_journee(day2)
+    print(joueur1.personal_record)
 # user
