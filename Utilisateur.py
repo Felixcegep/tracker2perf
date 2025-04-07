231943fb0991e1563260dca04438daf16d1563c6
felix <6244794@cegepoutaouais.qc.ca>
2025-04-06 21:40:25 -0500

    ajout de fonction mettre a jour pr dans instance

--- Utilisateur.py
+++ Utilisateur.py
@@ -1,12 +1,5 @@
-from Seance import Seance
-from Exercice import Exercice
 from Journee import Journee
-from Genre import Genre
-from datetime import datetime
 import pickle
-import enum
-from ExerciceCardio import ExerciceCardio
-from ExerciceMusculation import ExerciceMusculation
 # Todo: changer les methode d'instance pour avor le format
 # Todo: ajout de jsonpickle pour sauvegarder les progrès de l'utilisateur
 # Todo: verifier snake_case pour nom de variable
@@ -26,6 +19,7 @@ class Utilisateur:
         #Todo : tracker le poid de l'utilisateur lorsqu'il change avec la date
         self.poid = poid
         self.historique_journee = []
+        self.historique_poids_journee = []
 
         self.genre = genre
 
@@ -40,35 +34,36 @@ class Utilisateur:
         for journee in self.historique_journee:
             tupledate_poid = (journee.poid_aujourdhui, journee.date)
             liste_date_journee.append(tupledate_poid)
-        return liste_date_journee
-    
+        self.historique_poids_journee = liste_date_journee
+
     def actualiser_pr(self):
         pass
-        
 
+    def mettre_a_jours_pr(self,journee:Journee):
+        infojournee = journee.obtenir_exercices_info()
+        for exercice in infojournee:
+            if exercice["nom"] not in self.personal_record.keys():
+                print(exercice["nom"], "a ete ajouter au personal record")
+                self.personal_record[exercice["nom"]] = exercice["poid_kg"]
+
+            elif exercice["nom"] in self.personal_record.keys():
+                if exercice["poid_kg"] > self.personal_record[exercice["nom"]]:
+                    print("le pr a agmenter felicitation :)")
+                    self.personal_record[exercice["nom"]] = exercice["poid_kg"]
+                else:
+                    print("rien na changer dans pr")
     def ajouter_journee(self, journee:Journee):
-        #TODO : cette methode contiens deux fonctionnaliter change la 
+        #TODO : cette methode contiens deux fonctionnaliter change la
         # changer le poid parce que poid va etre dans journee
-        # de la maniere que pr a ete charger mais le faire dans un liste 
+        # de la maniere que pr a ete charger mais le faire dans un liste
         # historique de poid liste de tuple (date: poid)
         if isinstance(journee, Journee):
             self.historique_journee.append(journee)
-            
-            infojournee = journee.obtenir_exercices_info()
-            for exercice in infojournee:
-                if exercice["nom"] not in self.personal_record.keys():
-                    print(exercice["nom"],"a ete ajouter au personal record")
-                    self.personal_record[exercice["nom"]] = exercice["poid_kg"]
-                
-                elif exercice["nom"] in self.personal_record.keys():
-                    if exercice["poid_kg"] > self.personal_record[exercice["nom"]]:
-                        print("le pr a agmenter felicitation :)")
-                        self.personal_record[exercice["nom"]] = exercice["poid_kg"]
-                    else:
-                        print("rien na changer dans pr")
+            self.mettre_a_jours_pr(journee)
+
         else:
             raise ValueError("journee n'est pas une instance de Journee")
-                
+
     @classmethod
     def afficher_total_utilisateur(cls):
         print(cls.nombre_utilisateur)
@@ -84,9 +79,9 @@ class Utilisateur:
                        "set" : exercice.set,
                        "rep" : exercice.rep,
                        "poid_kg" : exercice.poid_kg,
-                        
+
                     }
-    
+
 
                     exercices_liste.append(exercice_individuel)
         return exercices_liste
