e64930c1bea9d778f5bd4b4198252e8479567376
felix <6244794@cegepoutaouais.qc.ca>
2025-04-12 03:45:43 -0500

    ajout de TODO

--- graphic_utilisateur.py
+++ graphic_utilisateur.py
@@ -13,7 +13,7 @@ class GraphicUtilisateur:
         self.info.actualiser_data_poid_jours()
         sorted_journees = sorted(self.info.historique_poids_journee, key=lambda x: x[1])
         print(sorted_journees)
-
+    # graphique volume de la sessions
 if __name__ == '__main__':
     g = GraphicUtilisateur()
     g.poid_journee()
