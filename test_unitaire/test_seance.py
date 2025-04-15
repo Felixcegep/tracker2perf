945ddf44e9a2c29667812448498411ef9df7f295
felix <6244794@cegepoutaouais.qc.ca>
2025-04-14 23:24:03 -0500

    ajout de test unitaire pour seance

---
+++ test_unitaire/test_seance.py
@@ -0,0 +1,33 @@
+from Exercice import Exercice
+from ExerciceCardio import ExerciceCardio
+from ExerciceMusculation import ExerciceMusculation
+from Seance import Seance
+import pytest
+
+
+def test_seanche_volume_par_seance():
+    Exercice.ajouter_mouvement_disponible("test","test",["Pectoraux"], "musculation")
+    exercice1 = ExerciceMusculation("test",5,5,5,5)
+    seance1 = Seance("test")
+    seance1.ajouter_exercice(exercice1)
+    assert seance1.volume_par_seance() == 125
+    exercice2 = ExerciceMusculation("test", 5, 5, 5, 5)
+    seance1.ajouter_exercice(exercice2)
+    assert seance1.volume_par_seance() == 250
+
+def test_ajouter_exercice():
+    exercice1 = ExerciceMusculation("test", 5, 5, 5, 5)
+    seance1 = Seance("test")
+    assert len(seance1.exercice_seaces) == 0
+    seance1.ajouter_exercice(exercice1)
+    assert len(seance1.exercice_seaces) == 1
+
+
+def test_supprimer_exercice():
+    exercice1 = ExerciceMusculation("test", 5, 5, 5, 5)
+    seance1 = Seance("test")
+    assert len(seance1.exercice_seaces) == 0
+    seance1.ajouter_exercice(exercice1)
+    assert len(seance1.exercice_seaces) == 1
+    seance1.supprimer_exercice("test")
+    assert len(seance1.exercice_seaces) == 0
