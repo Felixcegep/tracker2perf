ea25c4822b01c6fad9ea2a5d3702539e5d184bb3
felix <6244794@cegepoutaouais.qc.ca>
2025-04-15 04:09:42 -0500

    init dans exercice

---
+++ Muscu/__init__.py
@@ -0,0 +1,5 @@
+from .Exercice import Exercice
+from .Mouvement import Mouvement
+from .ExerciceMusculation import ExerciceMusculation
+from .ExerciceCardio import ExerciceCardio
+from .Seance import Seance
