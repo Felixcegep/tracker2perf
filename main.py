d3959bb84bc93b2a69cfc2d9d660d457a80d713b
felix <6244794@cegepoutaouais.qc.ca>
2025-04-15 19:49:16 -0500

    changement de import

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import pickle
 from datetime import timedelta, datetime
-from ExerciceMusculation import ExerciceMusculation
+from Muscu import ExerciceMusculation, Seance, Exercice
 from Utilisateur import Utilisateur
-from Seance import Seance
 from Journee import Journee
-from Exercice import Exercice
 from graphic_utilisateur import GraphicUtilisateur
 if __name__ == '__main__':
 
     g = GraphicUtilisateur()
-    g.volume_par_seance()
+    g.volume_par_seance(200)
     
     #ExerciceMusculation("test", 9,9,9,9)
-    """
+"""
     User12 = Utilisateur("test", 199,23,241,"homme")
     holy = datetime.today()
     holy -= timedelta(days = 1)
@@ -20,7 +18,7 @@ if __name__ == '__main__':
 
     seance_aujourd = Seance("journee_aujourd", )
 
-    W
+    
     seance_aujourd.ajouter_exercice(ExerciceMusculation("Développé couché", 5,5,5,5))
     seance_aujourd.ajouter_exercice (ExerciceMusculation("test",5,4,3,10))
     seance_aujourd.ajouter_exercice(ExerciceMusculation("test", 5, 4, 3, 16))
