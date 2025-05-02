7a11a517e496e5a7507155a9c0c73b12125546b6
loic <2050096@cegepoutaouais.qc.ca>
2025-05-02 14:25:54 -0400

    tests unitaire nutrition quotidien

--- test_unitaire/test_NutritionQuotidien.py
+++ test_unitaire/test_NutritionQuotidien.py
@@ -1,10 +1,27 @@
 import pytest
+from Nourriture import NutritionQuotidien, PortionAliment, TemplateAliment
 
 def test_ajouter_aliment_disponible():
-    pass
+
+    lundi = NutritionQuotidien("lundi")
+    len_initiale = len(lundi.aliment_ajourdhui)
+
+    PortionAliment.ajouter_aliment_disponible("yaourt", 100, 100)
+    NutritionQuotidien.ajouter_aliment_ajourdhui(lundi,PortionAliment("yaourt",150))
+
+    assert len_initiale == len(lundi.aliment_ajourdhui)-1
 
 def test_supprimer_aliment_disponible():
-    pass
+
+    lundi = NutritionQuotidien("lundi")
+
+    NutritionQuotidien.ajouter_aliment_ajourdhui(lundi, PortionAliment("yaourt", 150))
+    len_apres_ajout = len(lundi.aliment_ajourdhui)
+
+    NutritionQuotidien.supprimer_aliment_ajourdhui(lundi, "yaourt")
+    len_apres_supprimer = len(lundi.aliment_ajourdhui)
+
+    assert len_apres_supprimer == len_apres_ajout-1
 
 def test_sauvegarder_aliment_disponible():
     pass
